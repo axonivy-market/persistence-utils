5fc57a8af3a059501184e989af72919c132c2c85
Peter Stieglecker <peter.stieglecker@axonivy.com>
2024-06-27 17:57:52 +0200

    APS-222 added Swing DatabaseManager of HSQLB for JUnit tests

--- persistence-utils-demo-test/src_test/com/axonivy/utils/persistence/test/dao/CarDAOTest.java
+++ persistence-utils-demo-test/src_test/com/axonivy/utils/persistence/test/dao/CarDAOTest.java
@@ -1,18 +1,25 @@
 package com.axonivy.utils.persistence.test.dao;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.axonivy.utils.persistence.dao.QuerySettings;
 import com.axonivy.utils.persistence.daos.CarDAO;
 import com.axonivy.utils.persistence.entities.Car;
 import com.axonivy.utils.persistence.entities.Car_;
-import com.axonivy.utils.persistence.test.IvyTestBase;
+import com.axonivy.utils.persistence.test.DemoTestBase;
 
+import ch.ivyteam.ivy.environment.AppFixture;
 import ch.ivyteam.ivy.environment.IvyTest;
 
 
 @IvyTest
-public class CarDAOTest extends IvyTestBase {
+public class CarDAOTest extends DemoTestBase {
+
+	@BeforeEach
+	public void prepare(AppFixture fixture) throws Exception {
+		prepareTestDataAndMocking(true);
+	}
 
 	/**
 	 * Test problem described in TG-781
