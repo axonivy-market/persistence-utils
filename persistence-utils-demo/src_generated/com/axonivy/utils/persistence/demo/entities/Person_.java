b62f74f84f23d499798f5f5fceb1640b641dca37
Hoan Nguyen <hoan.nguyen01@axonactive.com>
2025-10-16 11:50:49 +0700

    Fix for demo

--- persistence-utils-demo/src_generated/com/axonivy/utils/persistence/demo/entities/Person_.java
+++ persistence-utils-demo/src_generated/com/axonivy/utils/persistence/demo/entities/Person_.java
@@ -1,33 +1,105 @@
 package com.axonivy.utils.persistence.demo.entities;
 
+import com.axonivy.utils.persistence.beans.AuditableIdEntity_;
 import com.axonivy.utils.persistence.demo.enums.MaritalStatus;
-import java.math.BigDecimal;
-import java.sql.Date;
-import javax.annotation.Generated;
+import jakarta.persistence.metamodel.EntityType;
 import jakarta.persistence.metamodel.SingularAttribute;
 import jakarta.persistence.metamodel.StaticMetamodel;
+import java.math.BigDecimal;
+import java.sql.Date;
 
-@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+/**
+ * Static metamodel for {@link com.axonivy.utils.persistence.demo.entities.Person}
+ **/
 @StaticMetamodel(Person.class)
-public abstract class Person_ extends com.axonivy.utils.persistence.beans.AuditableIdEntity_ {
+public abstract class Person_ extends AuditableIdEntity_ {
 
-	public static volatile SingularAttribute<Person, String> firstName;
-	public static volatile SingularAttribute<Person, String> lastName;
-	public static volatile SingularAttribute<Person, Date> birthdate;
-	public static volatile SingularAttribute<Person, String> ivyUserName;
-	public static volatile SingularAttribute<Person, Boolean> syncToIvy;
-	public static volatile SingularAttribute<Person, BigDecimal> salary;
-	public static volatile SingularAttribute<Person, Department> department;
-	public static volatile SingularAttribute<Person, MaritalStatus> maritalStatus;
-
+	
+	/**
+	 * @see #firstName
+	 **/
 	public static final String FIRST_NAME = "firstName";
+	
+	/**
+	 * @see #lastName
+	 **/
 	public static final String LAST_NAME = "lastName";
-	public static final String BIRTHDATE = "birthdate";
+	
+	/**
+	 * @see #ivyUserName
+	 **/
 	public static final String IVY_USER_NAME = "ivyUserName";
+	
+	/**
+	 * @see #syncToIvy
+	 **/
 	public static final String SYNC_TO_IVY = "syncToIvy";
-	public static final String SALARY = "salary";
-	public static final String DEPARTMENT = "department";
+	
+	/**
+	 * @see #birthdate
+	 **/
+	public static final String BIRTHDATE = "birthdate";
+	
+	/**
+	 * @see #maritalStatus
+	 **/
 	public static final String MARITAL_STATUS = "maritalStatus";
+	
+	/**
+	 * @see #salary
+	 **/
+	public static final String SALARY = "salary";
+	
+	/**
+	 * @see #department
+	 **/
+	public static final String DEPARTMENT = "department";
+
+	
+	/**
+	 * Static metamodel type for {@link com.axonivy.utils.persistence.demo.entities.Person}
+	 **/
+	public static volatile EntityType<Person> class_;
+	
+	/**
+	 * Static metamodel for attribute {@link com.axonivy.utils.persistence.demo.entities.Person#firstName}
+	 **/
+	public static volatile SingularAttribute<Person, String> firstName;
+	
+	/**
+	 * Static metamodel for attribute {@link com.axonivy.utils.persistence.demo.entities.Person#lastName}
+	 **/
+	public static volatile SingularAttribute<Person, String> lastName;
+	
+	/**
+	 * Static metamodel for attribute {@link com.axonivy.utils.persistence.demo.entities.Person#ivyUserName}
+	 **/
+	public static volatile SingularAttribute<Person, String> ivyUserName;
+	
+	/**
+	 * Static metamodel for attribute {@link com.axonivy.utils.persistence.demo.entities.Person#syncToIvy}
+	 **/
+	public static volatile SingularAttribute<Person, Boolean> syncToIvy;
+	
+	/**
+	 * Static metamodel for attribute {@link com.axonivy.utils.persistence.demo.entities.Person#birthdate}
+	 **/
+	public static volatile SingularAttribute<Person, Date> birthdate;
+	
+	/**
+	 * Static metamodel for attribute {@link com.axonivy.utils.persistence.demo.entities.Person#maritalStatus}
+	 **/
+	public static volatile SingularAttribute<Person, MaritalStatus> maritalStatus;
+	
+	/**
+	 * Static metamodel for attribute {@link com.axonivy.utils.persistence.demo.entities.Person#salary}
+	 **/
+	public static volatile SingularAttribute<Person, BigDecimal> salary;
+	
+	/**
+	 * Static metamodel for attribute {@link com.axonivy.utils.persistence.demo.entities.Person#department}
+	 **/
+	public static volatile SingularAttribute<Person, Department> department;
 
 }
 
